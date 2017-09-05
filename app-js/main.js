ab11fc5110f581939184f26b2efb277460ee3fea
Zhouxiang <895252509@qq.com>
2017-09-05 10:15:23 +0800

    1

---
+++ app-js/main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var stage = new Hilo.Stage({
+        renderType: 'canvas',
+        container: containerElem,
+        width: 320,
+        height: 480
+    });
+}
